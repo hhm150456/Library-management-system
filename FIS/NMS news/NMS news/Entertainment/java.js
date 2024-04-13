fb347f7ffe26ee969c0451207960aec820b02497
hhm150456 <hagarhisham2004@gmail.com>
2024-04-13 09:34:38 +0200

    Nms news 1

---
+++ FIS/NMS news/NMS news/Entertainment/java.js
@@ -0,0 +1,11 @@
+
+
+    function openSidebar() {
+    document.getElementById("sidebar").style.width = "250px";
+    document.getElementById("container").style.marginLeft = "250px";
+}
+
+function closeSidebar() {
+    document.getElementById("sidebar").style.width = "0";
+    document.getElementById("container").style.marginLeft = "0";
+};
